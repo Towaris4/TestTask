c69a3f78e45d2e1c59d94d79044fd3924e04719d
Towaris4 <ivan.tarasov2488@gmail.com>
2023-11-05 15:18:28 +0300

    for Anatoliy

---
+++ src/Firecalc.java
@@ -0,0 +1,26 @@
+
+public class Firecalc {
+    public static double remains(double percent, int year) { //функция находит значение остатка баланса на бирже
+        double capital = 1;
+        double expenditure = capital * percent / 100;
+        double balance = capital;
+        for (int i = year; i < 2022; i++) {
+            balance = balance - expenditure;
+            double income = (Constants.MOEX_RATE[i - 2001] / Constants.MOEX_RATE[i - 2002]);
+            balance = balance * income;
+            expenditure = expenditure * (1 + Constants.INFLATION_RATE[i - 2002] / 100);
+        }
+        return balance;
+    }
+    public static double FindPercent(int year) { //функция определяет процент при котором баланс к началу 2022 = 0
+        double a,b,d;
+        double percent = 0.001;
+        while (d > 0.001) {
+            a = remains(percent,year);
+            b = remains(,year);
+        }
+    }
+}
+
+
+
