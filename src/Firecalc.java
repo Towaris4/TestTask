541e46a19ff45d07909aca0485f25c8e9fa2e527
Towaris4 <ivan.tarasov2488@gmail.com>
2023-11-07 18:16:59 +0300

    for Anatoliy

--- src/Firecalc.java
+++ src/Firecalc.java
@@ -1,4 +1,10 @@
+import java.util.InputMismatchException;
 
+class InvalidInputException extends Exception {
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
 public class Firecalc {
     public static double remains(double percent, int year) { //функция находит значение остатка баланса на бирже на 2022
         double capital = 1;
@@ -12,7 +18,11 @@ public class Firecalc {
         }
         return balance;
     }
-    public static double FindPercent(int year) { //функция определяет процент при котором баланс к началу 2022 >= 0 методом половинного деления
+
+    public static double FindPercent(int year) throws InvalidInputException, InputMismatchException { //функция определяет процент при котором баланс к началу 2022 >= 0 методом половинного деления
+        if (year > 2021 || year < 2002) {
+            throw new InvalidInputException("Input year [2021-2022]");
+        }
         double a = 0;
         double b = 101;
         double eps = 0.0001;
@@ -22,17 +32,17 @@ public class Firecalc {
             double fb = remains(b, year);
             double fc = remains(c, year);
             if (fa * fc < 0) {
-                  b = c;
-             } else if (fb * fc < 0) {
-                  a = c;
+                b = c;
+            } else if (fb * fc < 0) {
+                a = c;
             }
         }
         double solution;
         solution = (a + b) / 2;
         return solution;
     }
-
 }
+
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,23 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Input: ");
-        int year = in.nextInt();
-        in.close();
-        double percent = Firecalc.FindPercent(year)-Firecalc.FindPercent(year)%0.5;
-        System.out.print("Input:" + percent);
+        try {
+            int year = in.nextInt();
+            double percent = Firecalc.FindPercent(year);
+            System.out.print("Output:" + (percent-percent%0.5) + "\n");
+        }
+        catch (InvalidInputException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Exception was processed. Program continues");
+        }
+        catch (InputMismatchException s) {
+            System.out.println(s.getMessage());
+            System.out.println("Exception was processed. Program continues");
+        }
         }
     }
