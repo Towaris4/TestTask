13f43b935ef10b30b5e0a946aeb22ab515546751
Towaris4 <ivan.tarasov2488@gmail.com>
2023-11-13 18:34:36 +0300

    for Anatoliy

--- src/FireCalc.java
+++ src/FireCalc.java
@@ -15,24 +15,6 @@ public class FireCalc {
         if (year > 2021 || year < 2002) {
             throw new InvalidInputException("Input year [2002-2021]");
         }
-        /*double minPercentValue = 0;
-        double maxPercentValue = 101;
-        double CALCULATION_ERROR = 0.0001;
-        while (Math.abs(maxPercentValue - minPercentValue) > CALCULATION_ERROR) {
-            double midPercentValue = (minPercentValue + maxPercentValue) / 2;
-            double functionOfMinPercentValue = getRemainsOn2022(minPercentValue, year);
-            double functionOfMaxPercentValue = getRemainsOn2022(maxPercentValue, year);
-            double functionOfMidPercentValue = getRemainsOn2022(midPercentValue, year);
-            if (functionOfMinPercentValue * functionOfMidPercentValue < 0) {
-                maxPercentValue = midPercentValue;
-            } else if (functionOfMaxPercentValue * functionOfMidPercentValue < 0) {
-                minPercentValue = midPercentValue;
-            }
-        }
-        double solution;
-        solution = (minPercentValue + maxPercentValue) / 2;
-        return solution;
-    }*/
         double percentValue = 0;
         double balanceOn2022 = getRemainsOn2022(percentValue, year);
         while (Double.compare(balanceOn2022, 0) != -1) {
